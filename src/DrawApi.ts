f231a736da415c21e83b90026ae2e49206fa0516
Beetroot Paul <paul@beetrootpaul.com>
2023-07-05 19:03:31 +0200

    temporary simplified implementation of sprite drawing

--- src/DrawApi.ts
+++ src/DrawApi.ts
@@ -14,10 +14,12 @@ export class DrawApi {
     this.#canvasRgbaBytes = canvasRgbaBytes;
   }
 
+  // TODO: cover it with tests
   setCameraOffset(offset: Xy): void {
     this.#cameraOffset = offset.round();
   }
 
+  // TODO: cover it with tests
   clear(color: Color): void {
     for (let pixel = 0; pixel < this.#canvasRgbaBytes.length / 4; pixel += 1) {
       const b = pixel * 4;
@@ -57,46 +59,16 @@ export class DrawApi {
 
   // TODO: remove this temporary method
   drawSomething(
-    pos = 0,
-    color: Color,
     imgBytes: Uint8Array,
-    imgW: number
+    imgW: number,
+    imgType: "rgb" | "rgba"
   ): void {
-    const pxLen = this.#canvasRgbaBytes.length / 4;
-
-    this.#canvasRgbaBytes[0] = 0;
-    this.#canvasRgbaBytes[1] = 0;
-    this.#canvasRgbaBytes[2] = 255;
-    this.#canvasRgbaBytes[3] = 255;
-
-    this.#canvasRgbaBytes[(pxLen - 1) * 4] = 0;
-    this.#canvasRgbaBytes[(pxLen - 1) * 4 + 1] = 255;
-    this.#canvasRgbaBytes[(pxLen - 1) * 4 + 2] = 0;
-    this.#canvasRgbaBytes[(pxLen - 1) * 4 + 3] = 255;
-
-    const ii = [
-      pos,
-      pos + 1,
-      pos + 2,
-      pos + this.#canvasSize.x,
-      pos + this.#canvasSize.x + 2,
-      pos + 2 * this.#canvasSize.x,
-      pos + 2 * this.#canvasSize.x + 1,
-      pos + 2 * this.#canvasSize.x + 2,
-    ].map((i) => i * 4);
-    ii.forEach((i) => {
-      this.#canvasRgbaBytes[i] = color.r;
-      this.#canvasRgbaBytes[i + 1] = color.g;
-      this.#canvasRgbaBytes[i + 2] = color.b;
-      this.#canvasRgbaBytes[i + 3] = 255;
-    });
-
-    // TODO: refactor
-    for (let px = 0; px < imgBytes.length / 4; px += 1) {
-      const offset = pos * 4 + Math.floor(px / imgW) * this.#canvasSize.x * 4;
+    const imgBytesPerColor = imgType === "rgb" ? 3 : 4;
+    for (let px = 0; px < imgBytes.length / imgBytesPerColor; px += 1) {
+      const offset = Math.floor(px / imgW) * this.#canvasSize.x * 4;
       const target = offset + (px % imgW) * 4;
-      const idx = px * 4;
-      if (imgBytes[idx + 3] > 127) {
+      const idx = px * imgBytesPerColor;
+      if (imgType === "rgb" || imgBytes[idx + 3] > 127) {
         this.#canvasRgbaBytes[target] = imgBytes[idx];
         this.#canvasRgbaBytes[target + 1] = imgBytes[idx + 1];
         this.#canvasRgbaBytes[target + 2] = imgBytes[idx + 2];
