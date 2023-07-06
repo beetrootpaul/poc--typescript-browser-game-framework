c1c9919e4ac6e0d9c3389f24ffb63449737b1ffc
Beetroot Paul <paul@beetrootpaul.com>
2023-07-06 23:14:08 +0200

    mention potential need for a non-spamming logger

--- src/game_loop/FpsLogger.ts
+++ src/game_loop/FpsLogger.ts
@@ -10,6 +10,7 @@ export class FpsLoggerAverage implements FpsLogger {
   readonly #samples: number[] = Array.from({ length: 60 });
   #nextIndex: number = 0;
 
+  // TODO: consider creation of a generic logger which could be used in a game in order to avoid spamming with `console.log`s every frame
   track(fps: number): void {
     this.#samples[this.#nextIndex++] = fps;
     this.#nextIndex = this.#nextIndex % this.#samples.length;
