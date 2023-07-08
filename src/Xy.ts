2be913d0f0bb40360c2be5c330199516930c12f2
Beetroot Paul <paul@beetrootpaul.com>
2023-07-08 17:04:00 +0200

    Xy#abs and Xy#magnitude

--- src/Xy.ts
+++ src/Xy.ts
@@ -16,16 +16,32 @@ export class Xy implements PrintDebug {
     this.y = y;
   }
 
-  min(): number {
-    return Math.min(this.x, this.y);
+  // TODO: cover with tests
+  magnitude(): number {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  abs(): Xy {
+    return new Xy(Math.abs(this.x), Math.abs(this.y));
+  }
+
+  floor(): Xy {
+    return new Xy(Math.floor(this.x), Math.floor(this.y));
   }
 
   round(): Xy {
     return new Xy(Math.round(this.x), Math.round(this.y));
   }
 
-  floor(): Xy {
-    return new Xy(Math.floor(this.x), Math.floor(this.y));
+  min(): number {
+    return Math.min(this.x, this.y);
+  }
+
+  clamp(xy1: Xy, xy2: Xy): Xy {
+    return new Xy(
+      Utils.clamp(xy1.x, this.x, xy2.x),
+      Utils.clamp(xy1.y, this.y, xy2.y)
+    );
   }
 
   add(other: Xy | number): Xy {
@@ -50,13 +66,6 @@ export class Xy implements PrintDebug {
     return typeof other === "number"
       ? new Xy(this.x / other, this.y / other)
       : new Xy(this.x / other.x, this.y / other.y);
-  }
-
-  clamp(xy1: Xy, xy2: Xy): Xy {
-    return xy_(
-      Utils.clamp(xy1.x, this.x, xy2.x),
-      Utils.clamp(xy1.y, this.y, xy2.y)
-    );
   }
 
   d(): string {
