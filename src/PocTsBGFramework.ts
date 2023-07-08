36ea2b2c7b3c6134319c57876ebb620c2cbb853b
Beetroot Paul <paul@beetrootpaul.com>
2023-07-09 00:26:45 +0200

    toggleable debug

--- src/PocTsBGFramework.ts
+++ src/PocTsBGFramework.ts
@@ -3,20 +3,20 @@ import { Framework, type FrameworkOptions } from "./Framework.ts";
 import { GameInputEvent } from "./game_input/GameInput.ts";
 import { StorageApi } from "./StorageApi.ts";
 
-let framework: Framework;
-
 //  This class is only a facade over other capabilities of this game framework.
 //    It serves as a public, global, statically accessible API.
 //    Inspiration: [PICO-8's API](https://www.lexaloffle.com/dl/docs/pico-8_manual.html).
 
 // TODO: after name for the framework gets chosen: rename this to the framework name or to its abbreviation
 export class PocTsBGFramework {
+  static #framework: Framework;
+
   // The most important function, has to be called first in order
   //   to properly initialize other fields and variables.
   //
 
   static init(options: FrameworkOptions): void {
-    framework = new Framework(options);
+    this.#framework = new Framework(options);
   }
 
   // Framework's lifecycle methods, exposed for a static access.
@@ -24,15 +24,15 @@ export class PocTsBGFramework {
   //
 
   static setOnUpdate(onUpdate: () => void) {
-    framework.setOnUpdate(onUpdate);
+    this.#framework.setOnUpdate(onUpdate);
   }
 
   static setOnDraw(onDraw: () => void): void {
-    framework.setOnDraw(onDraw);
+    this.#framework.setOnDraw(onDraw);
   }
 
   static startGame(onStart?: () => void): void {
-    framework.startGame(onStart);
+    this.#framework.startGame(onStart);
   }
 
   // The rest of the globally and statically available API.
@@ -43,4 +43,11 @@ export class PocTsBGFramework {
   static drawApi: DrawApi;
   static gameInputEvents: Set<GameInputEvent>;
   static storageApi: StorageApi;
+
+  // Debug flag.
+  //
+
+  static get debug(): boolean {
+    return this.#framework.debug;
+  }
 }
