0c1898b9ae3b8292f4aded85bd1e4cbf1aeb1c38
Beetroot Paul <paul@beetrootpaul.com>
2023-07-21 15:42:54 +0200

    adjust ellipse drawing algorithm based on reading http://members.chello.at/easyfilter/bresenham.html

--- src/draw_api/DrawEllipse.ts
+++ src/draw_api/DrawEllipse.ts
@@ -16,67 +16,69 @@ export class DrawEllipse {
     this.#pixel = new DrawPixel(this.#canvasBytes, this.#canvasSize);
   }
 
-  // TEST: for switched order of xy1 and xy2 (and soltion maybe this? -> [xy1,xy2]=[Math.min(…),Math.max(…)]
-  // Based on https://github.com/aseprite/aseprite/blob/25fbe786f8353a2ddb57de3bcc5db00066cc9ca6/src/doc/algo.cpp#L216-L315
+  // Based on http://members.chello.at/easyfilter/bresenham.html
   draw(
     xy1: Xy,
     xy2: Xy,
     color: SolidColor,
     fill: boolean,
+    // TODO: implement fill pattern for the ellipse
     fillPattern: FillPattern = FillPattern.primaryOnly
   ): void {
     if (Math.abs(xy2.x - xy1.x) <= 0 || Math.abs(xy2.y - xy1.y) <= 0) {
       return;
     }
 
-    let x0 = Math.min(xy1.x, xy2.x);
-    let x1 = Math.max(xy1.x, xy2.x) - 1;
-    let y0 = Math.min(xy1.y, xy2.y);
-    let y1 = Math.max(xy1.y, xy2.y) - 1;
-    const h = y1 - y0 + 1;
+    // swap coordinates to make sure xy1 is the left-bottom corner and xy2 is the right-top one
+    [xy1, xy2] = [
+      xy_(Math.min(xy1.x, xy2.x), Math.min(xy1.y, xy2.y)),
+      xy_(Math.max(xy1.x, xy2.x), Math.max(xy1.y, xy2.y)),
+    ];
 
-    // diameter
-    let a = Math.abs(x1 - x0);
-    let b = Math.abs(y1 - y0);
+    //
+    // PREPARE
+    //
 
+    let a = xy2.x - xy1.x - 1;
+    let b = xy2.y - xy1.y - 1;
     let b1 = b & 1;
 
-    // error increments
-    let dx = 4 * (1 - a) * b * b;
-    let dy = 4 * (b1 + 1) * a * a;
+    let left = xy1.x;
+    let right = xy2.x - 1;
+    let bottom = xy1.y + Math.floor((b + 1) / 2);
+    let top = bottom - b1;
 
-    // error of 1.step
-    let err = dx + dy + b1 * a * a;
-    let e2;
+    let errIncrementX = 4 * (1 - a) * b * b;
+    let errIncrementY = 4 * (b1 + 1) * a * a;
+    let currentErr = errIncrementX + errIncrementY + b1 * a * a;
 
-    y0 += Math.floor((b + 1) / 2);
-    // starting pixel
-    y1 = y0 - b1;
     a = 8 * a * a;
     b1 = 8 * b * b;
 
-    while (true) {
+    do {
+      //
+      // DRAW NEXT PIXEL IN EACH QUADRANT
+      //
+
       // TODO: update the implementation below to honor fill pattern
-      this.#pixel.draw(xy_(x1, y0), color); //   I. Quadrant
-      this.#pixel.draw(xy_(x0, y0), color); //  II. Quadrant
-      this.#pixel.draw(xy_(x0, y1), color); // III. Quadrant
-      this.#pixel.draw(xy_(x1, y1), color); //  IV. Quadrant
+      this.#pixel.draw(xy_(right, bottom), color);
+      this.#pixel.draw(xy_(left, bottom), color);
+      this.#pixel.draw(xy_(left, top), color);
+      this.#pixel.draw(xy_(right, top), color);
       if (fill) {
         // TODO: update the implementation below to honor fill pattern
-        //  I. & II. Quadrant
         Xy.forEachIntXyWithinRectOf(
-          xy_(x0 + 1, y0),
-          xy_(x1 - 1, y0).add(1),
+          xy_(left + 1, bottom),
+          xy_(right - 1, bottom).add(1),
           true,
           (xy) => {
             this.#pixel.draw(xy, color);
           }
         );
         // TODO: update the implementation below to honor fill pattern
-        //  III. & IV. Quadrant
         Xy.forEachIntXyWithinRectOf(
-          xy_(x0 + 1, y1),
-          xy_(x1 - 1, y1).add(1),
+          xy_(left + 1, top),
+          xy_(right - 1, top).add(1),
           true,
           (xy) => {
             this.#pixel.draw(xy, color);
@@ -84,37 +86,41 @@ export class DrawEllipse {
         );
       }
 
-      e2 = 2 * err;
-      // y step
-      if (e2 <= dy) {
-        y0 += 1;
-        y1 -= 1;
-        dy += a;
-        err += dy;
-      }
-      // x step
-      if (e2 >= dx || 2 * err > dy) {
-        x0 += 1;
-        x1 -= 1;
-        dx += b1;
-        err += dx;
-      }
+      //
+      // STEP TO THE NEXT PIXEL
+      //
 
-      if (x0 > x1) {
-        break;
+      const currentErrBeforeStep = currentErr;
+      if (2 * currentErrBeforeStep <= errIncrementY) {
+        bottom += 1;
+        top -= 1;
+        errIncrementY += a;
+        currentErr += errIncrementY;
       }
-    }
+      if (
+        2 * currentErrBeforeStep >= errIncrementX ||
+        2 * currentErr > errIncrementY
+      ) {
+        left += 1;
+        right -= 1;
+        errIncrementX += b1;
+        currentErr += errIncrementX;
+      }
+    } while (left <= right);
 
-    // TODO: cover this with tests
-    while (y0 - y1 < h) {
+    //
+    // DRAW MISSING TOP & BOTTOM PARTS
+    //
+
+    // TODO: Cover this with tests
+    while (bottom - top < b) {
       // TODO: update the implementation below to honor fill pattern
-      // too early stop of flat ellipses a=1
-      this.#pixel.draw(xy_(x0 - 1, y0), color);
-      this.#pixel.draw(xy_(x1 + 1, y0), color);
-      y0 += 1;
-      this.#pixel.draw(xy_(x0 - 1, y1), color);
-      this.#pixel.draw(xy_(x1 + 1, y1), color);
-      y1 -= 1;
+      this.#pixel.draw(xy_(left - 1, bottom), color);
+      this.#pixel.draw(xy_(right + 1, bottom), color);
+      bottom += 1;
+      this.#pixel.draw(xy_(left - 1, top), color);
+      this.#pixel.draw(xy_(right + 1, top), color);
+      top -= 1;
     }
   }
 }
